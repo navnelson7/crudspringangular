3b1ed685699c1f7ad32bef4572e326759125a0f6
navnelson7 <navnelson7@gmail.com>
2022-02-12 21:20:28 -0600

    creating listar and add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListarComponent } from './persona/listar/listar.component';
+import { AddComponent } from './persona/add/add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule,
