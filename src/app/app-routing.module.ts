3494a263484b96dc1803897441f9425fe6ca8617
navnelson7 <navnelson7@gmail.com>
2022-02-12 21:31:53 -0600

    add rotue to navigate at the component listar and add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddComponent } from './persona/add/add.component';
+import { EditComponent } from './persona/edit/edit.component';
+import { ListarComponent } from './persona/listar/listar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'listar', component:ListarComponent},
+  {path:'add', component:AddComponent},
+  {path:'edit', component:EditComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ListarComponent } from './persona/listar/listar.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'crudspringangular';
+  
+  constructor(private router:Router){}
+
+  Listar(){
+    this.router.navigate(['listar']);
+  }
+
+  Nuevo(){
+    this.router.navigate(['add']);
+  }
 }
